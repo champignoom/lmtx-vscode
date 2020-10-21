90369900acaf9893829c82491d5f2a10cd25c40f
champignoom <champignoom@gmail.com>
2020-10-21 22:31:07 +0200

    refactor

---
+++ src/output_channel.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+
+let myOutputChannel: vscode.OutputChannel | undefined = undefined;
+
+function getFreshOutputChannel(): vscode.OutputChannel {
+	if (myOutputChannel === undefined) {
+		myOutputChannel = vscode.window.createOutputChannel("lmtx");
+	} else {
+		myOutputChannel.clear();
+	}
+
+	myOutputChannel.show(true);
+	return myOutputChannel;
+}
+
+export {
+    getFreshOutputChannel,
+}
