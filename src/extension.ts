d4222251fc65d7e0f0c7dc17bc2c45a8a02495ae
champignoom <champignoom@gmail.com>
2020-10-22 15:34:36 +0200

    add a snippet for code highlighting and command for showing available file types

--- src/extension.ts
+++ src/extension.ts
@@ -7,9 +7,58 @@ import * as path from 'path'
 import { assert } from 'console';
 import { findSourceMap } from 'module';
 import { resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
+// import { ExtensionContext, StatusBarAlignment, window, StatusBarItem, Selection, workspace, TextEditor, commands, ProgressLocation } from 'vscode';
 
 import { myOutputChannel, getFreshOutputChannel } from './output_channel'
 import { cmdPickFont } from './font_picker'
+
+function cmdPrintSupportedVimFileType(context: vscode.ExtensionContext) {
+	const work = () => {
+		// vim/nvim can only print buffer content and options to stdout in ex mode by default
+		// to print anything else, verbose mode (-V1) is necessary
+		// The random string is to identify (the line before) the result among the noises.
+		const randomString = Math.random().toString();	
+		const result = child_process.spawnSync('nvim', [
+			'-V1',
+			'-es',
+			`+echo "${randomString}"`,
+			'+echo getcompletion("", "filetype")',
+			'+set sw',
+			'+q',
+		]);
+
+		const showError = () => {vscode.window.showErrorMessage("failed to obtain supported file types");};
+		if (result.status !== 0) {
+			return showError();
+		}
+
+		const lines = result.stderr.toString().split(/\r?\n/);
+		const randomStringIdx = lines.indexOf(randomString);
+		if (randomStringIdx<0 || randomStringIdx>=lines.length-1) {
+			return showError();
+		}
+
+		const resultWithDoubleQuotes = lines[randomStringIdx+1].replace(/'/g, '"');
+		const resultArray = JSON.parse(resultWithDoubleQuotes) as Array<string>
+		const outputChannel = getFreshOutputChannel();
+		for (let x of resultArray) {
+			outputChannel.appendLine(x);
+		}
+	}
+
+	const progress = vscode.window.withProgress({
+		location: vscode.ProgressLocation.Window,
+		title: "fetching supported file types",
+		cancellable: false
+	}, (progress, token) => {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				work();
+				resolve();
+			}, 0);
+		});
+	});
+}
 
 function cmdCompile(context: vscode.ExtensionContext) {
 	const outputChannel = getFreshOutputChannel();
@@ -64,6 +113,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('context-lmtx.pickfonts', () => cmdPickFont(context)));
 	context.subscriptions.push(vscode.commands.registerCommand("context-lmtx.compile", () => cmdCompile(context)));
+	context.subscriptions.push(vscode.commands.registerCommand("context-lmtx.print-vim-filetypes", () => cmdPrintSupportedVimFileType(context)));
 }
 
 // this method is called when your extension is deactivated
